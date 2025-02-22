37d1b5d52c708619558a86c953491c67e30cc395
AnhQuocs <anhquocydkl@gmail.com>
2025-02-22 11:47:46 +0700

    row

--- app/src/main/java/com/example/syt/MainActivity.kt
+++ app/src/main/java/com/example/syt/MainActivity.kt
@@ -4,29 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Visibility
-import androidx.compose.material.icons.filled.VisibilityOff
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material.icons.filled.Save
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -34,16 +22,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.syt.ui.theme.SYTTheme
 
 class MainActivity : ComponentActivity() {
@@ -65,144 +45,117 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HomeScreenApp() {
-    var email by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
+//    var email by remember { mutableStateOf("") }
+//    var password by remember { mutableStateOf("") }
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        Image(
-            painter = painterResource(id = R.drawable.picwhite),
-            contentDescription = "",
-            modifier = Modifier.fillMaxSize(),
-            contentScale = ContentScale.Crop
-        )
-    }
+//    Box(modifier = Modifier.fillMaxSize()) {
+//        Image(
+//            painter = painterResource(id = R.drawable.picwhite),
+//            contentDescription = "",
+//            modifier = Modifier.fillMaxSize(),
+//            contentScale = ContentScale.Crop
+//        )
+//    }
 
     Column(
-        modifier = Modifier.padding(24.dp, 96.dp, 24.dp, 24.dp),
+        modifier = Modifier.padding(12.dp, 96.dp, 12.dp, 24.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            "Welcome Back",
-            fontSize = 52.sp,
-            textAlign = TextAlign.Center,
-            color = Color(0xFF33CC66)
-        )
-
-        Spacer(modifier = Modifier.padding(top = 64.dp))
-
-        EmailTextField(email = email, onMailChange = {email = it})
-
-        Spacer(modifier = Modifier.padding(top = 24.dp))
-
-        PasswordTextField(password = password, onPasswordChange = {password = it})
-
-        Spacer(modifier = Modifier.padding(top = 24.dp))
-
-        LoginButton()
-
-        Row (
-            modifier = Modifier.padding(0.dp, 24.dp)
-        ) {
-            Text("Don't have a account? ", fontSize = 19.sp)
-            Text(
-                "Sign up.",
-                color = Color(0xFF33CC66),
-                fontSize = 19.sp,
-                textDecoration = TextDecoration.Underline,
-                modifier = Modifier.clickable {  }
-            )
+        Row (verticalAlignment = Alignment.CenterVertically) {
+            TextField(value = "", onValueChange = {}, modifier = Modifier.weight(1f))
+            Icon(Icons.Default.Save, contentDescription = "Save", Modifier.size(50.dp).padding(start = 6.dp))
         }
     }
 }
 
-@Composable
-fun EmailTextField(email : String, onMailChange : (String) -> Unit) {
-    Column () {
-        Text(
-            "Email Address",
-            fontSize = 18.sp,
-            modifier = Modifier
-                .align(Alignment.Start)
-                .padding(start = 4.dp, top = 36.dp)
-        )
-
-        OutlinedTextField(
-            value = email,
-            onValueChange = onMailChange,
-            label = {Text("Email Address", color = Color.Gray)},
-            leadingIcon = {
-                Icon(
-                    Icons.Default.Email,
-                    contentDescription = "",
-                    tint = Color(0xFF33CC66)
-                )
-            },
-            modifier = Modifier.fillMaxWidth(),
-            shape = MaterialTheme.shapes.medium
-        )
-    }
-}
-
-
-@Composable
-fun PasswordTextField(password : String, onPasswordChange : (String)  -> Unit) {
-    var isShowPassword by remember {
-        mutableStateOf(false)
-    }
-    Column () {
-        Text(
-            "Password",
-            fontSize = 18.sp,
-            modifier = Modifier
-                .align(Alignment.Start)
-                .padding(start = 4.dp)
-        )
-        OutlinedTextField(
-            value = password,
-            onValueChange = onPasswordChange,
-            label = {Text("Password", color = Color.Gray)},
-            leadingIcon = {
-                Icon(
-                    Icons.Default.Lock,
-                    contentDescription = "",
-                    tint = Color(0xFF33CC66)
-                )
-            },
-            trailingIcon = {
-                IconButton(
-                    onClick = {
-                        isShowPassword = !isShowPassword
-                    }
-                ) {
-                    Icon(
-                        if(isShowPassword) Icons.Filled.VisibilityOff else Icons.Filled.Visibility,
-                        contentDescription = "",
-                        tint = Color(0xFF33CC66)
-                    )
-                }
-            },
-            modifier = Modifier.fillMaxWidth(),
-            shape = MaterialTheme.shapes.medium,
-            visualTransformation = if(isShowPassword) VisualTransformation.None else PasswordVisualTransformation()
-        )
-    }
-
-}
-
-@Composable
-fun LoginButton() {
-    Button(
-        onClick = {},
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color(0xFF33CC66)
-        ),
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(50.dp)
-    ) {
-        Text("Login", fontSize = 22.sp, textAlign = TextAlign.Center)
-    }
-}
+//@Composable
+//fun EmailTextField(email : String, onMailChange : (String) -> Unit) {
+//    Column () {
+//        Text(
+//            "Email Address",
+//            fontSize = 18.sp,
+//            modifier = Modifier
+//                .align(Alignment.Start)
+//                .padding(start = 4.dp, top = 36.dp)
+//        )
+//
+//        OutlinedTextField(
+//            value = email,
+//            onValueChange = onMailChange,
+//            label = {Text("Email Address", color = Color.Gray)},
+//            leadingIcon = {
+//                Icon(
+//                    Icons.Default.Email,
+//                    contentDescription = "",
+//                    tint = Color(0xFF33CC66)
+//                )
+//            },
+//            modifier = Modifier.fillMaxWidth(),
+//            shape = MaterialTheme.shapes.medium
+//        )
+//    }
+//}
+//
+//
+//@Composable
+//fun PasswordTextField(password : String, onPasswordChange : (String)  -> Unit) {
+//    var isShowPassword by remember {
+//        mutableStateOf(false)
+//    }
+//    Column () {
+//        Text(
+//            "Password",
+//            fontSize = 18.sp,
+//            modifier = Modifier
+//                .align(Alignment.Start)
+//                .padding(start = 4.dp)
+//        )
+//        OutlinedTextField(
+//            value = password,
+//            onValueChange = onPasswordChange,
+//            label = {Text("Password", color = Color.Gray)},
+//            leadingIcon = {
+//                Icon(
+//                    Icons.Default.Lock,
+//                    contentDescription = "",
+//                    tint = Color(0xFF33CC66)
+//                )
+//            },
+//            trailingIcon = {
+//                IconButton(
+//                    onClick = {
+//                        isShowPassword = !isShowPassword
+//                    }
+//                ) {
+//                    Icon(
+//                        if(isShowPassword) Icons.Filled.VisibilityOff else Icons.Filled.Visibility,
+//                        contentDescription = "",
+//                        tint = Color(0xFF33CC66)
+//                    )
+//                }
+//            },
+//            modifier = Modifier.fillMaxWidth(),
+//            shape = MaterialTheme.shapes.medium,
+//            visualTransformation = if(isShowPassword) VisualTransformation.None else PasswordVisualTransformation()
+//        )
+//    }
+//
+//}
+//
+//@Composable
+//fun LoginButton() {
+//    Button(
+//        onClick = {},
+//        colors = ButtonDefaults.buttonColors(
+//            containerColor = Color(0xFF33CC66)
+//        ),
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .height(50.dp)
+//    ) {
+//        Text("Login", fontSize = 22.sp, textAlign = TextAlign.Center)
+//    }
+//}
 
 @Preview(showBackground = true)
 @Composable
